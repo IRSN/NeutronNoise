1d71b0ff6e586ef41b7f8d883a63d268756f0d94
wilfried <wil.mon@protonmail.com>
2020-11-18 14:58:20 +0100

    tests xcr

--- tests/testthat/test-xcr.R
+++ tests/testthat/test-xcr.R
@@ -20,11 +20,15 @@ test_that("Normal", {
   
   din <- data.table::fread(file.path("data", "ntrac_000.ncap"))
   
+  set.seed(1)
+  
   xout <- expect_error(as_signal(din, x_duration = 1000, is_t0 = TRUE) %>% feynman_hist(samples_widths = c(0.8, 0.9)) %>% xcr(), NA)
   
   expect_equal(xout$samples_width, c(0.8, 0.9))
   
   expect_equal(xout$Y1, c(1.809, 1.809), tolerance = 1e-3)
+  
+  expect_equal(xout$Y2, c(0.01300760, 0.02086617), tolerance = 1e-3)
   
 })
 
